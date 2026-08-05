dd2ac3760dd2e9777c1d47ea6d73ba1b9ae01d10
SimonF <simonfrancoguisado@gmail.com>
2026-08-05 15:42:28 -0500

    Subiendo el estilo de las cards

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -10,8 +10,6 @@ const descripcion = document.getElementById("descripcion");
 
 const listaTareas = document.getElementById("listaTareas");
 
-let tareas = [];
-
 botonAgregar.addEventListener("click", saleFormulario);
 botonCancelar.addEventListener("click", guardarFormulario);
 botonGuardar.addEventListener("click", agregarTarea);
@@ -30,55 +28,3 @@ function guardarFormulario(){
     fecha.value = "";
     descripcion.value = "";
 }
-
-function agregarTarea() {
-
-    if(
-        nombre.value.trim() === "" ||
-        fecha.value === "" ||
-        descripcion.value.trim() === ""
-    ){
-        alert("Debe rellenar todos los campos.");
-        return;
-    }
-
-    let nuevaTarea = {
-        nombre: nombre.value,
-        fecha: fecha.value,
-        descripcion: descripcion.value
-    }
-
-    tareas.push(nuevaTarea);
-    mostrarTareas();
-    guardarFormulario();
-}
-
-function mostrarTareas() {
-
-    listaTareas.innerHTML = "";
-
-    for (let i = 0; i < tareas.length; i++) {
-        let tarjeta = document.createElement("div");
-
-        tarjeta.classList.add("tarea")
-
-        tarjeta.innerHTML = `
-            <div class="tarea-header">
-                <h4>${tareas[i].nombre}</h4>
-
-                <div class="acciones">
-                    <span class="fecha">${tareas[i].fecha}</span>
-
-                        <button>✓</button>
-                        <button>🗑</button>
-                </div>
-            </div>
-
-            <p>${tareas[i].descripcion}</p>
-        `;
-
-        listaTareas.appendChild(tarjeta);
-    }
-}
-
-
